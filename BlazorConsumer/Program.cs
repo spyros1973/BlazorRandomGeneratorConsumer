4b17177771649136484b122b5bfd15ddd7651aed
Spyros Paraschis <sparaschis@hotmail.com>
2022-10-24 00:39:53 +0300

    Generated data are now kept when navigating to a different app page.

--- BlazorConsumer/Program.cs
+++ BlazorConsumer/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ApiCommunicator>();
+builder.Services.AddSingleton<ContentHolder>();
 
 var app = builder.Build();
 
---
+++ BlazorConsumer/Services/ContentHolder.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Reflection.Metadata;
+
+public class ContentHolder
+    {
+    public static List<string> JobTitles { get; set; } = new();
+    public static string PoliticalSpeech { get; set; } = "";
+
+    }
