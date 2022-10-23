4f74b41d25feb7362df92856a65b9688aff362c2
Spyros Paraschis <sparaschis@hotmail.com>
2022-10-23 20:07:05 +0300

    Add project files.

---
+++ BlazorConsumer/Services/ApiCommunicator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class ApiCommunicator
+    {
+        const string baseUrl = "https://generator20221016203403.azurewebsites.net/"; //"https://contentgenerator.azurewebsites.net/"; // bogustitlesgr?count=10";
+        public static async Task<string> GetJobTitle(string lang = "")
+        {
+            HttpClient client = new HttpClient();
+            return await client.GetStringAsync($"{baseUrl}bogustitle{lang}");
+        }
+
+        public static async Task<string> GetPoliticalSpeech()
+        {
+            HttpClient client = new HttpClient();
+            return await client.GetStringAsync($"{baseUrl}woodenlanguagegr");
+        }
+    }
